98df4b80a58973e5eb8553ad395fdcbd2941d322
Gopi Krishna Menon <krishnagopi487.github@outlook.com>
2021-10-30 23:12:37 +0530

    Add Immediate Class
    Added Immediate Class for storing immediate values.

---
+++ include/assembler/immediate.hpp
@@ -0,0 +1,26 @@
+#include <bitset>
+#include <range/v3/all.hpp>
+
+namespace assembler {
+
+struct Immediate {
+ private:
+  std::bitset<64> binary_value;
+  std::int64_t m_value;
+  std::int32_t m_operand_length;
+
+ public:
+  Immediate() = default;
+  Immediate( std::int64_t value, std::int32_t operand_length = 12 )
+      : m_value( value ), m_operand_length( operand_length ) {}
+  std::int64_t getValue() { return m_value; }
+
+  std::int32_t getOperandLength() { return m_operand_length; }
+
+  std::string getBinaryValue() {
+    binary_value = m_value;
+    return binary_value.to_string() |
+           ranges::action::drop( 64 - m_operand_length );
+  }
+};
+}  // namespace assembler
--- test/assembler/immediate.cpp
+++ test/assembler/immediate.cpp
@@ -1,9 +1,28 @@
 #define BOOST_TEST_MODULE immediate_operand
 
+#include <assembler/immediate.hpp>
 #include <boost/test/unit_test.hpp>
+
+using namespace assembler;
 
 BOOST_AUTO_TEST_SUITE( immediate_operand_methods )
 
-BOOST_AUTO_TEST_CASE( test ) {}
+BOOST_AUTO_TEST_CASE( get_value ) {
+  Immediate test_operand( 487 );
+
+  BOOST_REQUIRE_EQUAL( test_operand.getValue(), 487 );
+}
+
+BOOST_AUTO_TEST_CASE( get_operand_length ) {
+  Immediate test_operand( 0, 12 );
+
+  BOOST_REQUIRE_EQUAL( test_operand.getOperandLength(), 12 );
+}
+
+BOOST_AUTO_TEST_CASE( get_binary_value ) {
+  Immediate test_operand( 5, 20 );
+
+  BOOST_REQUIRE_EQUAL( test_operand.getBinaryValue(), "00000000000000000101" );
+}
 
 BOOST_AUTO_TEST_SUITE_END()
