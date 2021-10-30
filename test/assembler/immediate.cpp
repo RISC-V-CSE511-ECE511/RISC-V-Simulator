edf75bd675e36296af875c220cd4c4a969fc557d
Gopi Krishna Menon <krishnagopi487.github@outlook.com>
2021-10-30 22:07:47 +0530

    Add Test Scaffolding
    Add Test Scaffolding for future testing of components

---
+++ test/assembler/immediate.cpp
@@ -0,0 +1,9 @@
+#define BOOST_TEST_MODULE immediate_operand
+
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE( immediate_operand_methods )
+
+BOOST_AUTO_TEST_CASE( test ) {}
+
+BOOST_AUTO_TEST_SUITE_END()
---
+++ test/assembler/instruction.cpp
@@ -0,0 +1,9 @@
+#define BOOST_TEST_MODULE assembler_test
+
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE( instruction_methods )
+
+BOOST_AUTO_TEST_CASE( test ) {}
+
+BOOST_AUTO_TEST_SUITE_END()
