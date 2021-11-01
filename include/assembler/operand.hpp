f08941cc5947c99e077e1c11b23f7006cb1270df
Gopi Krishna Menon <krishnagopi487.github@outlook.com>
2021-11-01 07:47:47 +0530

    Add method to identify OperandType in Operand.hpp

--- include/assembler/operand.hpp
+++ include/assembler/operand.hpp
@@ -4,6 +4,10 @@
 
 namespace assembler {
 struct Operand {
+  enum OperandType { RegisterOp, ImmediateOp };
+  static OperandType identifyType( std::string_view operand ) {
+    return operand[0] == 'r' ? RegisterOp : ImmediateOp;
+  }
   virtual std::string getBinaryValue() = 0;
   virtual ~Operand(){};
 };
