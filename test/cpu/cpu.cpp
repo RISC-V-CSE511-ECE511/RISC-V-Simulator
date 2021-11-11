9d4472422a31898bec473e43464833f314c0fbdd
Gopi Krishna Menon <krishnagopi487.github@outlook.com>
2021-11-11 09:23:19 +0530

    Abstract out state information from CPU

--- test/cpu/cpu.cpp
+++ test/cpu/cpu.cpp
@@ -1,4 +1,6 @@
 #include <functional>
+
+#include "cpu/decoder/connection_info.hpp"
 #define BOOST_TEST_MODULE cpu_test
 
 #include <boost/test/unit_test.hpp>
@@ -7,49 +9,55 @@
 
 namespace cpu {
 
-struct Cpu {
- private:
-  std::string m_memory = "";
-  std::int32_t m_register_file[31] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                                       0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                                       0, 0, 0, 0, 0, 0, 0, 0, 0 };
+struct State {
+  std::string memory = "";
+  std::int32_t register_file[31] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                     0, 0, 0, 0, 0, 0, 0, 0, 0 };
   std::int32_t PC = 0;
   std::string IR = "";
+};
 
- public:
+struct Executor {
   // R Type
-  static void add_func( Cpu* cpu, const ConnectionInfo& conn_info ) {
-    int* rf = cpu->getRegisterFile();
+ private:
+  static void add_func( State& sys_state, const ConnectionInfo& conn_info ) {
+    int* rf = sys_state.register_file;
     rf[conn_info.operand1] = rf[conn_info.operand2] + rf[conn_info.operand3];
   }
-  static void sub_func( Cpu* cpu, const ConnectionInfo& conn_info ) {
-    int* rf = cpu->getRegisterFile();
+  static void sub_func( State& sys_state, const ConnectionInfo& conn_info ) {
+    int* rf = sys_state.register_file;
     rf[conn_info.operand1] = rf[conn_info.operand2] - rf[conn_info.operand3];
   }
-  static void xor_func( Cpu* cpu, const ConnectionInfo& conn_info ) {
-    int* rf = cpu->getRegisterFile();
+  static void xor_func( State& sys_state, const ConnectionInfo& conn_info ) {
+    int* rf = sys_state.register_file;
     rf[conn_info.operand1] = rf[conn_info.operand2] ^ rf[conn_info.operand3];
   }
-  static void and_func( Cpu* cpu, const ConnectionInfo& conn_info ) {
-    int* rf = cpu->getRegisterFile();
+  static void and_func( State& sys_state, const ConnectionInfo& conn_info ) {
+    int* rf = sys_state.register_file;
     rf[conn_info.operand1] = rf[conn_info.operand2] & rf[conn_info.operand3];
   }
-  static void or_func( Cpu* cpu, const ConnectionInfo& conn_info ) {
-    int* rf = cpu->getRegisterFile();
+  static void or_func( State& sys_state, const ConnectionInfo& conn_info ) {
+    int* rf = sys_state.register_file;
     rf[conn_info.operand1] = rf[conn_info.operand2] | rf[conn_info.operand3];
   }
-  static void sll_func( Cpu* cpu, const ConnectionInfo& conn_info ) {
-    int* rf = cpu->getRegisterFile();
+  static void sll_func( State& sys_state, const ConnectionInfo& conn_info ) {
+    int* rf = sys_state.register_file;
     rf[conn_info.operand1] = rf[conn_info.operand2] << rf[conn_info.operand3];
   }
-  static void sra_func( Cpu* cpu, const ConnectionInfo& conn_info ) {
-    int* rf = cpu->getRegisterFile();
+  static void sra_func( State& sys_state, const ConnectionInfo& conn_info ) {
+    int* rf = sys_state.register_file;
     rf[conn_info.operand1] = rf[conn_info.operand2] >> rf[conn_info.operand3];
+  }
+
+ public:  // API
+  static void execute( State& sys_state, const ConnectionInfo& conn_info ) {
+    m_instr_func_map[conn_info.instr_name]( sys_state, conn_info );
   }
 
  private:
   static inline std::unordered_map<
-      std::string, std::function<void( cpu::Cpu*, const ConnectionInfo& )>>
+      std::string, std::function<void( State&, const ConnectionInfo& )>>
       m_instr_func_map{ { "add", std::bind( &add_func, std::placeholders::_1,
                                             std::placeholders::_2 ) },
                         { "sub", std::bind( &sub_func, std::placeholders::_1,
@@ -64,42 +72,38 @@ struct Cpu {
                                             std::placeholders::_2 ) },
                         { "sra", std::bind( &sra_func, std::placeholders::_1,
                                             std::placeholders::_2 ) } };
+};
+
+struct CPU {
+ private:
+  State sys_state;
 
  public:
-  int* getRegisterFile() { return m_register_file; }
-
-  std::int32_t getPC() { return PC; }
-  void setPC( std::int32_t new_PC ) { PC = new_PC; }
-
-  std::string getIR() { return IR; }
-  void setIR( std::string new_IR ) { IR = new_IR; }
-
   void runProgram( const std::string& program ) {
     loadProgram( program );
     // Logic for this needs to be changed
-    while ( PC != m_memory.size() ) {
+    while ( sys_state.PC != sys_state.memory.size() ) {
       // fetch Instruction
-      IR = fetchInstruction();
+      sys_state.IR = fetchInstruction();
       // Decode Instruction and Get Connection Information
-      ConnectionInfo conn_info = Decoder::decode( IR );
+      ConnectionInfo conn_info = Decoder::decode( sys_state.IR );
       // Execute Instruction
-      execute( conn_info );
+      Executor::execute( sys_state, conn_info );
     }
-  }
-
-  void execute( const ConnectionInfo& conn_info ) {
-    m_instr_func_map[conn_info.instr_name]( this, conn_info );
   }
 
   std::string fetchInstruction() {
     std::string instruction;
-    instruction = std::string( m_memory.begin() + PC,
-                               m_memory.begin() + PC + 32 );  // Getting 4 bytes
-    PC += 32;
+    instruction = std::string(
+        sys_state.memory.begin() + sys_state.PC,
+        sys_state.memory.begin() + sys_state.PC + 32 );  // Getting 4 bytes
+    sys_state.PC += 32;
     return instruction;
   }
 
-  void loadProgram( const std::string& program ) { m_memory = program; }
+  void loadProgram( const std::string& program ) { sys_state.memory = program; }
+
+  State& getSystemState() { return sys_state; }
 };
 
 }  // namespace cpu
@@ -109,7 +113,7 @@ using namespace cpu;
 BOOST_AUTO_TEST_SUITE( cpu_test_suite )
 
 BOOST_AUTO_TEST_CASE( fetch_instruction ) {
-  Cpu test_cpu;
+  CPU test_cpu;
   std::string program =
       "0000000000110001000000001011001101000000001100010000000010110011";
   test_cpu.loadProgram( program );
@@ -124,10 +128,10 @@ BOOST_AUTO_TEST_SUITE_END()
 
 BOOST_AUTO_TEST_SUITE( r_type_test_suite )
 BOOST_AUTO_TEST_CASE( add_test ) {
-  Cpu test_cpu;
+  CPU test_cpu;
   std::string program = "00000000001100010000000010110011";
 
-  int* rf = test_cpu.getRegisterFile();
+  int* rf = test_cpu.getSystemState().register_file;
 
   rf[2] = 10;
   rf[3] = 20;
@@ -138,10 +142,10 @@ BOOST_AUTO_TEST_CASE( add_test ) {
 }
 
 BOOST_AUTO_TEST_CASE( sub_test ) {
-  Cpu test_cpu;
+  CPU test_cpu;
   std::string program = "01000000001100010000000010110011";
 
-  int* rf = test_cpu.getRegisterFile();
+  int* rf = test_cpu.getSystemState().register_file;
 
   rf[2] = 10;
   rf[3] = 20;
@@ -152,10 +156,10 @@ BOOST_AUTO_TEST_CASE( sub_test ) {
 }
 
 BOOST_AUTO_TEST_CASE( xor_test ) {
-  Cpu test_cpu;
+  CPU test_cpu;
   std::string program = "00000000001100010100000010110011";
 
-  int* rf = test_cpu.getRegisterFile();
+  int* rf = test_cpu.getSystemState().register_file;
 
   rf[2] = 10;
   rf[3] = 20;
@@ -166,10 +170,10 @@ BOOST_AUTO_TEST_CASE( xor_test ) {
 }
 
 BOOST_AUTO_TEST_CASE( and_test ) {
-  Cpu test_cpu;
+  CPU test_cpu;
   std::string program = "00000000001100010111000010110011";
 
-  int* rf = test_cpu.getRegisterFile();
+  int* rf = test_cpu.getSystemState().register_file;
 
   rf[2] = 10;
   rf[3] = 20;
@@ -180,10 +184,10 @@ BOOST_AUTO_TEST_CASE( and_test ) {
 }
 
 BOOST_AUTO_TEST_CASE( or_test ) {
-  Cpu test_cpu;
+  CPU test_cpu;
   std::string program = "00000000001100010110000010110011";
 
-  int* rf = test_cpu.getRegisterFile();
+  int* rf = test_cpu.getSystemState().register_file;
 
   rf[2] = 10;
   rf[3] = 20;
@@ -194,10 +198,10 @@ BOOST_AUTO_TEST_CASE( or_test ) {
 }
 
 BOOST_AUTO_TEST_CASE( sll_test ) {
-  Cpu test_cpu;
+  CPU test_cpu;
   std::string program = "00000000001100010001000010110011";
 
-  int* rf = test_cpu.getRegisterFile();
+  int* rf = test_cpu.getSystemState().register_file;
 
   rf[2] = 10;
   rf[3] = 20;
@@ -208,10 +212,10 @@ BOOST_AUTO_TEST_CASE( sll_test ) {
 }
 
 BOOST_AUTO_TEST_CASE( sra_test ) {
-  Cpu test_cpu;
+  CPU test_cpu;
   std::string program = "01000000001100010101000010110011";
 
-  int* rf = test_cpu.getRegisterFile();
+  int* rf = test_cpu.getSystemState().register_file;
 
   rf[2] = 10;
   rf[3] = 20;
